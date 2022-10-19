d15d82239aa8cfbd862a8b28d67f2b85d4352a80
André Claudino <claudino@kognita.ai>
2022-10-19 11:26:17 -0300

    Change script to version 1.0.3

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-kubectl -n $NAMESPACE get pods | grep $POD_STATUS | awk '{print $1}' | xargs kubectl -n $NAMESPACE delete pods --force
-echo "Finished"
+function worker() {
+    kubectl -n $NAMESPACE get pods | grep $POD_STATUS | awk '{print $1}' | xargs kubectl -n $NAMESPACE delete pods --force
+    echo "Finished"
+}
+
+worker
+echo $?
